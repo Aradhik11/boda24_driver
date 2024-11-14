49a6be3553f156d18f3aee807b94e5ca2abcfc77
Muhammad Rabilu <35644353+muhammadrabilu@users.noreply.github.com>
2024-11-14 15:28:07 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/boda24/driver/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.boda24.driver
+
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.content.Intent
+
+class MainActivity: FlutterFragmentActivity() {
+    private val channel = "flutter.app/awake"
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channel).setMethodCallHandler {
+      call, result ->
+      // This method is invoked on the main thread.
+      // TODO
+      
+      if(call.method == "awakeapp"){
+          awakeapp()
+      }else{
+          println("message recieved in activityyyy")
+      }
+    }
+    }
+      private fun awakeapp(){
+        val bringToForegroundIntent = Intent(this, MainActivity::class.java);
+        if(bringToForegroundIntent != null){
+startActivity(bringToForegroundIntent);
+        }else{
+val launchIntent = getPackageManager().getLaunchIntentForPackage("com.package.name");
+if (launchIntent != null) { 
+    startActivity(launchIntent);//null pointer check in case com.package.name was not found
+}   }
+
+    }
+}
