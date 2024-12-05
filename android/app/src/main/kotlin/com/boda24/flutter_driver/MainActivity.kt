2e5af34c05e384ea3f95c824580227c6997e2c78
Dhikrullah Abdur-Rahmon <aradhik11@gmail.com>
2024-12-05 13:41:27 +0100

    ios fix

---
+++ android/app/src/main/kotlin/com/boda24/flutter_driver/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.boda24.flutter_driver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/yourdesiredorg/flutter_driver/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.yourdesiredorg.flutter_driver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
